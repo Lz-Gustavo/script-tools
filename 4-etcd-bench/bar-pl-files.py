bf6574e0756a19c4a2ce45cad7cc12b836d44ccb
Lz-Gustavo <xavier.luizgustavo@gmail.com>
2022-04-13 16:43:03 -0300

    boxplots for batch sizes on log file comparisons against SL

--- 4-etcd-bench/bar-pl-files.py
+++ 4-etcd-bench/bar-pl-files.py
@@ -21,9 +21,9 @@ def ExtractData(filename):
     return data
 
 
-def Plot(data, workloads, labels, xaxis, yaxis, filename):
+def PlotBar(data, workloads, labels, xaxis, yaxis, filename):
     xlabels = np.arange(len(labels))
-    width = 0.1
+    width = 0.2
 
     fig, ax = plt.subplots()
     ax.yaxis.grid(linestyle='--')
@@ -39,7 +39,21 @@ def Plot(data, workloads, labels, xaxis, yaxis, filename):
     ax.set_xticklabels(labels)
     fig.tight_layout()
 
-    plt.legend(loc='best')
+    plt.legend(loc='upper left')
+    plt.savefig(filename)
+
+
+def PlotBoxplot(data, labels, xaxis, yaxis, filename):
+    fig, ax = plt.subplots()
+    ax.yaxis.grid(linestyle='--')
+
+    ax.boxplot(data)
+    ax.set_ylabel(yaxis)
+    ax.set_xlabel(xaxis)
+
+    ax.set_xticklabels(labels)
+    fig.tight_layout()
+
     plt.savefig(filename)
 
 
@@ -53,7 +67,8 @@ def main():
                 root_folder + "/" + sub_folder + "/pl-1-10keys"
             ],
             "count_fname": "logcounts.out",
-            "size_fname": "logsize.out"
+            "size_fname": "logsize.out",
+            "batch_fname": "logbatches.out"
         },
         {
             "name": "PL-10",
@@ -62,7 +77,8 @@ def main():
                 root_folder + "/" + sub_folder + "/pl-10-10keys"
             ],
             "count_fname": "logcounts.out",
-            "size_fname": "logsize.out"
+            "size_fname": "logsize.out",
+            "batch_fname": "logbatches.out"
         },
         {
             "name": "PL-100",
@@ -71,7 +87,8 @@ def main():
                 root_folder + "/" + sub_folder + "/pl-100-10keys"
             ],
             "count_fname": "logcounts.out",
-            "size_fname": "logsize.out"
+            "size_fname": "logsize.out",
+            "batch_fname": "logbatches.out"
         },
         {
             "name": "PL-500",
@@ -80,16 +97,19 @@ def main():
                 root_folder + "/" + sub_folder + "/pl-500-10keys"
             ],
             "count_fname": "logcounts.out",
-            "size_fname": "logsize.out"
+            "size_fname": "logsize.out",
+            "batch_fname": "logbatches.out"
         }
     ]
 
     labels = []
     workload_counts = []
     workload_sizes = []
+    workload_batches = []
     for w in workloads:
         workload_counts.append([])
         workload_sizes.append([])
+        workload_batches.append([])
 
     for c in configs:
         paths = c["paths"]
@@ -102,22 +122,25 @@ def main():
             # command counts
             count_fname = paths[i] + "/" + c["count_fname"]
             data = ExtractData(count_fname)
-
             arr = np.array(data)
             diff =  1 - (np.sum(arr) / sl_command_count)
             workload_counts[i].append(diff)
 
-
             # logsize
             size_fname = paths[i] + "/" + c["size_fname"]
             data = ExtractData(size_fname)[0]
-
             diff = 1 - float(data / sl_byte_size)
-            #print(diff)
             workload_sizes[i].append(diff)
 
-    Plot(workload_counts, workloads, labels, 'Configurations', 'Command Reduction', 'test-counts.png')
-    Plot(workload_sizes, workloads, labels, 'Configurations', 'Log Size Reduction', 'test-sizes.png')
+            # batches
+            batch_fname = paths[i] + "/" + c["batch_fname"]
+            data = ExtractData(batch_fname)
+            workload_batches[i].append(data)
+
+    PlotBar(workload_counts, workloads, labels, 'Configurations', 'Command Reduction', 'log-counts.png')
+    PlotBar(workload_sizes, workloads, labels, 'Configurations', 'Log Size Reduction', 'log-sizes.png')
+    for i in range(0, len(workloads)):
+        PlotBoxplot(workload_batches[i], labels, 'Configurations', 'Log Batch Sizes', 'log-batches-' + workloads[i] + '.png')
 
 
 if __name__ == "__main__":
