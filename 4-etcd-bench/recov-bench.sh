013e5e1d7b16cab7a0c6546da5f2959a3bb1022a
LzGustavo <xavier.luizgustavo@gmail.com>
2022-06-16 12:03:13 -0300

    adjusting recovery benchmark script to correctly extract log files in a way etcd understands (tested on the cluster just now)

--- 4-etcd-bench/recov-bench.sh
+++ 4-etcd-bench/recov-bench.sh
@@ -30,14 +30,14 @@ nodeScript=${rootFolder}/run-singlenode.sh
 echo "running..."
 for (( i = 0; i < ${iterations}; i++ )); do
     echo "#${i}: preparing files"
-    ssh root@${etcdHostname} "mv ${logFiles} ${recovPath}; tar -xzvf ${recovPath}/logs.tar.gz; mv ${recovPath}/logs/* ${recovPath}"
+    ssh root@${etcdHostname} "cp ${logFiles} ${recovPath}; tar -xzvf ${recovPath}/logs.tar.gz -C ${recovPath} --strip-components 3; rm ${recovPath}/logs.tar.gz"
 
     echo "#${i}: launching server on remote"
     ssh root@${etcdHostname} "${nodeScript}" &
     sleep ${sleepDurationSec}s
 
     echo "#${i}: killing server on remote and copying results"
-    ssh root@${etcdHostname} "killall etcd -u root -w; mv ${measureFile} ${rootFolder}/${i}-recov-time.out"
+    ssh root@${etcdHostname} "killall etcd -u root -w; mv ${measureFile} ${rootFolder}/${i}-recov-time.out; rm ${recovPath}/*"
 
     echo "#${i}: finished iteration"; echo ""
 done
