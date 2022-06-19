7b8b96172cb9c3c25035da5b3177a232e2c6265a
LzGustavo <xavier.luizgustavo@gmail.com>
2022-06-19 16:16:40 -0300

    minor adjustments on recov time benchmark

--- 4-etcd-bench/recov-bench.sh
+++ 4-etcd-bench/recov-bench.sh
@@ -6,7 +6,7 @@ measureFile=/tmp/recov-time.out
 sleepDurationSec=10
 iterations=10
 
-recovPath=/tmp
+recovPath=/tmp/recov
 if [[ $# -lt 1 ]]; then
 	echo "using ${recovPath} value for 'recovPath'"
 
@@ -26,18 +26,19 @@ fi
 
 logFiles=${rootFolder}/logs.tar.gz
 nodeScript=${rootFolder}/run-singlenode.sh
+etcdPath=/media/disk1/etcd
 
 echo "running..."
 for (( i = 0; i < ${iterations}; i++ )); do
     echo "#${i}: preparing files"
-    ssh root@${etcdHostname} "cp ${logFiles} ${recovPath}; tar -xzvf ${recovPath}/logs.tar.gz -C ${recovPath} --strip-components 3; rm ${recovPath}/logs.tar.gz"
+    ssh root@${etcdHostname} "mkdir -p ${recovPath}; cp ${logFiles} ${recovPath}; tar -xzvf ${recovPath}/logs.tar.gz -C ${recovPath} --strip-components 3; rm ${recovPath}/logs.tar.gz"
 
     echo "#${i}: launching server on remote"
     ssh root@${etcdHostname} "${nodeScript}" &
     sleep ${sleepDurationSec}s
 
     echo "#${i}: killing server on remote and copying results"
-    ssh root@${etcdHostname} "killall etcd -u root -w; mv ${measureFile} ${rootFolder}/${i}-recov-time.out; rm ${recovPath}/*"
+    ssh root@${etcdHostname} "killall etcd -u root -w; mv ${measureFile} ${rootFolder}/${i}-recov-time.out; rm ${recovPath}/*; rm -r ${etcdPath}/*"
 
     echo "#${i}: finished iteration"; echo ""
 done
